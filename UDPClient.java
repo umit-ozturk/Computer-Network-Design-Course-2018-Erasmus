8e35c16a7a50103130c8f90601ddb4a2419df45e
Ümit <umit.ozturk.9716@gmail.com>
2018-04-16 20:43:16 +0300

    UDP Server and Client File Created

---
+++ UDPClient.java
@@ -0,0 +1,13 @@
+//UDPClient.java
+
+import java.net.*;
+import java.io.*;
+
+class UDPClient 
+{
+   public static void main(String args[]) throws Exception
+   {
+   	  
+
+   }
+}
---
+++ UDPServer.java
@@ -0,0 +1,13 @@
+//UDPServer.java
+
+import java.net.*;
+import java.io.*;
+
+
+class UDPServer 
+{
+   public static void main(String args[]) throws Exception
+      {
+
+      }
+}
