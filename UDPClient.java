3b3d38aeb1f40045feb60b9198e9cf87b52ae01c
Ümit <umit.ozturk.9716@gmail.com>
2018-04-16 20:44:41 +0300

    Created Sockets

--- UDPClient.java
+++ UDPClient.java
@@ -8,6 +8,16 @@ class UDPClient
    public static void main(String args[]) throws Exception
    {
    	  
-
+   	  byte[] send_data = new byte[1024];
+   	  
+      BufferedReader infromuser = 
+                        new BufferedReader(new InputStreamReader(System.in));
+                        
+      DatagramSocket client_socket = new DatagramSocket();
+      
+      InetAddress IPAddress =  InetAddress.getByName("127.0.0.1");
+           
+       
+      client_socket.close();
    }
 }
--- UDPServer.java
+++ UDPServer.java
@@ -8,6 +8,15 @@ class UDPServer
 {
    public static void main(String args[]) throws Exception
       {
-
+      	
+      	 byte[] receive_data = new byte[1024];
+         byte[] send_data = new byte[1024];
+         
+         int recv_port;
+         
+         DatagramSocket server_socket = new DatagramSocket(5000);
+         
+         System.out.println ("UDPServer Waiting for client on port 5000");
+         
       }
 }
