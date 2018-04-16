9102d0a7b58c40421d25641c439c314b52621042
Ümit <umit.ozturk.9716@gmail.com>
2018-04-16 21:33:45 +0300

    While Loop Created for Message Platform

--- UDPClient.java
+++ UDPClient.java
@@ -16,7 +16,32 @@ class UDPClient
       DatagramSocket client_socket = new DatagramSocket();
       
       InetAddress IPAddress =  InetAddress.getByName("127.0.0.1");
-           
+      
+      while (true)
+      {
+      	
+      	System.out.println("Type Something (q or Q to quit): ");
+      	
+      	String data = infromuser.readLine();
+      	
+      	if (data.equals("q") || data.equals("Q"))
+      	break;
+      	
+      	else
+      	
+      	{
+      		
+      	send_data = data.getBytes();
+      
+        DatagramPacket send_packet = new DatagramPacket(send_data,
+                                                        send_data.length, 
+                                                        IPAddress, 5000);
+                                                      
+        client_socket.send(send_packet);
+        
+        }
+       
+      }       
        
       client_socket.close();
    }
--- UDPServer.java
+++ UDPServer.java
@@ -18,5 +18,31 @@ class UDPServer
          
          System.out.println ("UDPServer Waiting for client on port 5000");
          
+              
+         while(true)
+         {
+          
+          DatagramPacket receive_packet = new DatagramPacket(receive_data,
+                                              receive_data.length);
+                                              
+                  server_socket.receive(receive_packet);
+                  
+                  String data = new String(receive_packet.getData(),0,0
+                                           ,receive_packet.getLength());
+                  
+                  InetAddress IPAddress = receive_packet.getAddress();
+                  
+                  recv_port = receive_packet.getPort();
+                  
+                  if (data.equals("q") || data.equals("Q"))
+                  break;
+                  
+                  else
+                  
+                  System.out.println("( " + IPAddress + " , " + recv_port 
+                  + " ) said :" + data );
+                  
+                  
+         }
       }
 }
